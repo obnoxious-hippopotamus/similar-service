b43fee04ca696bd447add2c741639c3f808370a1
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-03-12 15:11:35 -0600

    all data seeded and DB tables set correctly

--- schema.sql
+++ schema.sql
@@ -20,17 +20,21 @@ CREATE TABLE attributes (
   id SERIAL PRIMARY KEY, 
   movieId INT NOT NULL,
   title   TEXT NOT NULL,
-  supportingActors VARCHAR(100) NULL,
+  genre TEXT NOT NULL,
+  supportingActors1 VARCHAR(100) NULL,
+  supportingActors2 VARCHAR(100) NULL,
+  supportingActors3 VARCHAR(100) NULL,
   producers VARCHAR(100) NULL, 
   studio VARCHAR(100) NULL, 
-  subtitles VARCHAR(100) NULL,
+  subtitles TEXT NULL,
   audioLanguages VARCHAR(100) NULL, 
-  purchaseRights VARCHAR(100) NULL, 
-  formats VARCHAR(100) NULL, 
-  devices VARCHAR(100) NULL, 
+  purchaseRights TEXT NULL, 
+  formats TEXT NULL, 
+  devices TEXT NULL, 
   poster VARCHAR(100)
 
 );
 
 
 --  SEED DATA
+-- SEE SEED SCRIPT IN PACKAGE JSON
--- seed.js
+++ seed.js
@@ -1,6 +1,7 @@
 
 const axios = require('axios');
 const connection = require('./database/connection');
+var faker = require('faker');
 
 
  async function seedDbData() {
@@ -8,7 +9,7 @@ const connection = require('./database/connection');
   var movieIds = [];
   var page = 1;
   
-  while (page <= 5) {
+  while (page <= 6) {
     var moviesEndpoint = `https://api.themoviedb.org/3/discover/movie?api_key=5ee6c27316d948d0a1d5833299d57422&language=en-US&sort_by=popularity.desc&include_video=false&page=${page}`;
     await axios.get(moviesEndpoint)
       .then(response => {
@@ -20,14 +21,16 @@ const connection = require('./database/connection');
       .catch(err => console.log(err));
       page++
     }
-    // console.log(movieIds);
+
 
   for (var i = 0; i < movieIds.length; i++) {
     var descriptionEndpoint = `https://api.themoviedb.org/3/movie/${movieIds[i]}?api_key=5ee6c27316d948d0a1d5833299d57422&language=en-US`
     await axios.get(descriptionEndpoint)
     .then(response => {
-      console.log(response.data.poster_path);
-      var queryString = `INSERT INTO attributes(movieId, title, studio, audioLanguages, poster) VALUES(${response.data.id}, "${response.data.original_title}", "${response.data.production_companies[0].name}", "${response.data.original_language}", "${response.data.poster_path}")`;
+      console.log(response.data.genres[0]);
+      var queryString = `INSERT INTO attributes(movieId, title, studio, audioLanguages, poster, supportingActors1, supportingActors2, supportingActors3, producers, subtitles, purchaseRights, formats, devices, genre) 
+      VALUES(${response.data.id}, "${response.data.original_title}", "${response.data.production_companies[0].name}", "${response.data.original_language}", "${response.data.poster_path}", "${faker.name.findName()}",
+       "${faker.name.findName()}", "${faker.name.findName()}", "${faker.name.findName()}", "Yes", "Stream Instantly Details", "Prime Video (streaming online video)", "Available to watch on supported devices", "${response.data.genres[0].name}")`;
       connection.query(queryString, (err, results, fields) => {
         if (err) {
           console.log(err);
@@ -38,7 +41,6 @@ const connection = require('./database/connection');
     })
     .catch(err => {console.log(err)});
   }
-
 };
 
 seedDbData();
