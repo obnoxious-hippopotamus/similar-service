81862cb47437cb12ddaafd765e1ae401e35749f0
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-03-16 17:56:06 -0600

    More details component complete.  description component next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,10 +20,9 @@ class App extends React.Component {
           <main className="header" >Customers who watched this item also watched</main>
             <div className="wrapper" >
               <div className="movieWheel">Movie Wheel</div>
-              <div className="moreDetails" >More Details</div>
-              <div className="details">actors and what not</div>
+              <div className="moreDetails" >More details</div>
+              <Details className="details"/>
               <div className="description">Description and Information</div>
-              {/* <Details /> */}
             </div>
         </div>
       )
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Details = () => {
     return(
-        <p className="test" >Test</p>
+        <ul className="detailsProperties" >
+          <li>Supporting actors</li>
+          <li>Producers</li>
+          <li>MPAA rating</li>
+          <li>Subtitles</li>
+          <li>Audio Languages</li>
+          <li>Purchase Rights</li>
+          <li>Format</li>
+          <li>Devices</li>
+        </ul>
     )
 }
 
