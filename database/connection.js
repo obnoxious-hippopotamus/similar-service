e50a4fe695df8ef618ba62c7973b72b2f60a04a0
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-05-05 15:17:02 -0600

    adjusted connection to pool

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 
 //DATABASE CONNECTION - this how we are able to make queries to our DB
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+var connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 
 
 connection.connect(function(err) {
@@ -11,34 +11,12 @@ connection.connect(function(err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
-
   console.log('Houston, we have a connection! With ID:' + connection.threadId);
 });
 
 
 
-function handleDisconnect() {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
-                                                  // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
 
 
 
