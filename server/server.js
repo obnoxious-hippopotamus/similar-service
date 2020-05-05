35e7b2871480de91b3333ddaa9229f5c1f6ec640
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-05-04 18:08:32 -0600

    adjusted port number to accept processs.env

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //ROUTE IMPORT
 const movieRoutes = require('./routes/movieRoutes');
